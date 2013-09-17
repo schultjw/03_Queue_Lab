70eb3d14488c2d9fed5c9ea77fa8a02f0c361819
kojsmn <kojsmn@miamioh.edu>
2013-09-17 10:50:54 -0400

    Comments

--- Queue.h
+++ Queue.h
@@ -17,7 +17,7 @@ class Queue {
   //Syntax note: In C++ you don't put private/protected/public on each
   // individual member function or variable. Instead, we declare a section
   // as public, and now everything that comes afterward will be public, until
-  // we chage it. See ArrayQueue.h for another example.
+  // we change it. See ArrayQueue.h for another example.
  public:
 
   //Add the input item to the back of the queue
